7de3d59c7f6ab6ae2dabef8e43d8562517783c7f
yogi779 <yogesh_kumar21@outlook.com>
2025-04-10 10:37:21 +0530

    Add files via upload

--- back.js
+++ back.js
@@ -1,4 +1,3 @@
-// back.js
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -12,46 +11,56 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-const rooms = {}; // Store chat rooms
+const rooms = {}; // Store chat rooms and users
+const users = {}; // Store usernames by socket ID
 
-// On connection
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    // Join a room
-    socket.on('joinRoom', (roomName) => {
-        if (!rooms[roomName]) {
-            rooms[roomName] = { users: [] };
+    // Join room with username
+    socket.on('joinRoom', ({ room, username }) => {
+        users[socket.id] = username;
+
+        if (!rooms[room]) {
+            rooms[room] = { users: [] };
         }
 
-        rooms[roomName].users.push(socket.id);
-        socket.join(roomName);
-        console.log(`${socket.id} joined room: ${roomName}`);
+        rooms[room].users.push({ id: socket.id, name: username });
+        socket.join(room);
 
-        // Notify other users in the room
-        io.to(roomName).emit('roomUsers', rooms[roomName].users);
+        console.log(`${username} joined room: ${room}`);
+
+        // Send updated user list
+        const userList = rooms[room].users.map(user => user.name);
+        io.to(room).emit('roomUsers', userList);
     });
 
-    // Receive a message and broadcast to room
-    socket.on('sendMessage', (data) => {
-        io.to(data.room).emit('receiveMessage', {
-            user: socket.id,
-            message: data.message
+    // Send message to room
+    socket.on('sendMessage', ({ room, message }) => {
+        const username = users[socket.id] || "Unknown";
+        io.to(room).emit('receiveMessage', {
+            user: username,
+            message
         });
     });
 
-    // User disconnects
+    // Handle disconnect
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
+        const username = users[socket.id];
 
-        // Remove user from all rooms
-        for (const roomName in rooms) {
-            rooms[roomName].users = rooms[roomName].users.filter(user => user !== socket.id);
+        for (const room in rooms) {
+            rooms[room].users = rooms[room].users.filter(user => user.id !== socket.id);
+
+            // Send updated user list to room
+            const updatedUsers = rooms[room].users.map(user => user.name);
+            io.to(room).emit('roomUsers', updatedUsers);
         }
+
+        delete users[socket.id];
     });
 });
 
-// Starting the server
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
